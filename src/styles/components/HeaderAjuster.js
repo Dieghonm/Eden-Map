3783a517e3c244b229f41aa5024121455f229220
Diegho <dieghonm@gmail.com>
2025-12-23 18:09:00 -0300

    Rmove linhas de teste

--- src/styles/components/HeaderAjuster.js
+++ src/styles/components/HeaderAjuster.js
@@ -5,7 +5,7 @@ import { spacing } from '../../theme/texts';
 export const createStyles = (theme) =>
   StyleSheet.create({
     spacer:{
-      backgroundColor: '#0ABAB5',
+      // backgroundColor: '#0ABAB5',
       width: spacing.xxxl * 2,
       height: 70,
     }
